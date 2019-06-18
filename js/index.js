4f40f086a01d1ba3f37562e4dda24e51198d2701
lawsonarichard <51132990+lawsonarichard@users.noreply.github.com>
2019-06-18 15:47:18 -0700

    more work on index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,19 @@
+// mouse over to change color of NAV
 const navtextColor = document.querySelectorAll('.nav-link');
 navtextColor.forEach((navlink) => {
-    navlink.style.color = 'grey'
+    navlink.style.color = 'grey';
     navlink.addEventListener('mouseover', function() {
         navlink.style.color = 'blue';
+        navlink.style.textDecoration = 'line-through';
     })
     navlink.addEventListener('mouseout', function() {
          navlink.style.color = 'grey';
+         navlink.style.textDecoration = 'none';
     })
 
 });
 
+// add shadow to images on mouse over
 const images = document.querySelectorAll('img'); 
     images.forEach((img) => {
         img.addEventListener('mouseover', function() {
@@ -20,12 +24,37 @@ const images = document.querySelectorAll('img');
          })
         });
 
-        const text = document.querySelectorAll('h2'); 
-        text.forEach((bigText) => {
+// click on header text, become large then become small on mouseout
+const h2text = document.querySelectorAll('h2'); 
+        h2text.forEach((bigText) => {
             bigText.addEventListener('click', function() {
                 bigText.style.fontSize = '5rem';
             })
             bigText.addEventListener('mouseout', function() {
                 bigText.style.fontSize = '';
              })
-            });
+            }); 
+            
+// mouseover to change text color            
+const h4text = document.querySelectorAll('h4'); 
+    h4text.forEach((colorText) => {
+        colorText.addEventListener('dblclick', function() {
+            colorText.style.color = 'red';
+            colorText.backgroundColor = 'grey';
+            })
+            colorText.addEventListener('mouseout', function() {
+                colorText.style.color = 'black';
+             })
+            }); 
+            
+// click on button alert not yet functional
+
+const btnAlert = document.querySelectorAll('.btn')
+        btnAlert.forEach((btn) => {
+            btn.addEventListener('click', function() {
+                btn.style.background = 'red';
+                alert("This function is currently not yet functional");
+            })
+        });
+
+// footer click to change image 
